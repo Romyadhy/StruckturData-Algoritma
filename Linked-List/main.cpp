59d78021b4446e99e0d5e0d9948809a471995aa2
I Putu Romyadhy Mahaputra_2215101052 <127913223+Romyadhy@users.noreply.github.com>
2023-04-26 00:09:32 +0800

    Update main.cpp

--- Linked-List/main.cpp
+++ Linked-List/main.cpp
@@ -32,34 +32,34 @@ int main (int argc, char** argv)
 	list2.printAll();
 	
 	 // Insert to Tail
-    list1.insertToTail(8);
-    list1.insertToTail(3);
+    	list1.insertToTail(8);
+    	list1.insertToTail(3);
     
-    cout << "3. INSERT TO TAIL(8 and 3): " << endl;
-    list1.printAll();
+    	cout << "3. INSERT TO TAIL(8 and 3): " << endl;
+    	list1.printAll();
     
-    // Insert After
-    list1.insertAfter(15, 20);
+    	// Insert After
+    	list1.insertAfter(15, 20);
     
-    cout << "4. INSERT AFTER(insert 15 after 20): " << endl;
-    list1.printAll();
+   	cout << "4. INSERT AFTER(insert 15 after 20): " << endl;
+    	list1.printAll();
     
-    // Delete From Head
-    list1.deleteFromHead();
+    	// Delete From Head
+    	list1.deleteFromHead();
     
-    cout << "5. DELETE FROM HEAD(delete 100): " << endl;
-    list1.printAll();
+    	cout << "5. DELETE FROM HEAD(delete 100): " << endl;
+    	list1.printAll();
     
-     // Delete From Tail
-    list1.deleteFromTail();
-    cout << "6. DELETE FROM TAIL(delete 3): " << endl;
-    list1.printAll();
+     	// Delete From Tail
+    	list1.deleteFromTail();
+    	cout << "6. DELETE FROM TAIL(delete 3): " << endl;
+    	list1.printAll();
 
-    // Delete By Value
-    list1.deleteByValue(15);
+    	// Delete By Value
+    	list1.deleteByValue(15);
     
-    cout << "7. DELETE BY VALUE(delete 15): " << endl;
-    list1.printAll();
+    	cout << "7. DELETE BY VALUE(delete 15): " << endl;
+    	list1.printAll();
     
 	
 	return 0;
